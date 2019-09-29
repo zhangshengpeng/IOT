4f36cf1d5c21f3dd7f7ba696e08f3c157656182b
zhangshengpeng <1612977540@qq.com>
2019-09-30 05:01:26 +0800

    111

--- routes/mysql.js
+++ routes/mysql.js
@@ -31,7 +31,7 @@ exports.getIo = (io)=>{
             io.emit('init', data) 
         });
         socket.on('init',(imsi)=>{
-            let sql = `SELECT * FROM iot WHERE imsi = ${imsi} limit 10`
+            let sql = `SELECT * FROM iot WHERE imsi = ${imsi} where date order by date desc limit 10`
             connection.query(sql,(err,result)=>{
                 if(err){ console.log(err) 
                 } else {
@@ -74,7 +74,7 @@ exports.Insert = (data)=> {
         if(err){
             console.log(err)
         } else {
-            var sql = "SELECT date,temp,light from iot limit 10"
+            var sql = "SELECT date,temp,light from iot where date order by date desc limit 10"
             connection.query(sql,function(err,result){if (err) {console.log(err)}else{ 
             let Data = {
                 IMSI: data.IMSI,
